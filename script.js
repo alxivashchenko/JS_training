8e8edd10e01e27609d5115f9b9dbbab514f69d65
alxivashchenko <40021890+alxivashchenko@users.noreply.github.com>
2020-03-15 18:51:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var first = 1;
+
+let second = 2;
+console.log('second'/8);
+console.log(4/0);
+let person = {
+    name : "John",
+    age : 25,
+    isMarried : false
+};
+console.log(person.name);
+console.log(person["name"]);
+let answer = confirm("Are you here?");
+console.log(answer);
+let answer2 = prompt("Are you 18?", "Yes");
+console.log(answer2);
+console.log(typeof(answer2));
+let a = 2;
+console.log(a);
+a = 'Stringa';
+console.log(a);
+
+
+
+
